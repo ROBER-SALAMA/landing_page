c150f4b55d21671ff517b351b3d7eba7e295d199
Roberto Salama <102178835+ROBER-SALAMA@users.noreply.github.com>
2023-02-10 15:47:53 -0600

    le añadi un scrip al html

---
+++ html/main.js
@@ -0,0 +1,20 @@
+// define lass imagenes
+const imagenes = [
+    'img/carro1.jpg',
+    'img/carro2.jpg',
+    'img/dowload.jpg'
+]
+
+// app stado
+let index = 0;
+const max = imagenes.length;
+
+// interactuar con los elemento del DOM
+const contetElement = document.querySelector('.content');
+const preSlideButton = document.querySelector('.prevSlide');
+const nexSlideButton = document.querySelector('.nextSlide');
+
+// evento al dar click
+preSlideButton.addEventListener("click", function() {
+    
+})
